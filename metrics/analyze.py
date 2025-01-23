89746a9d9a9e8e90bbb6094d3ff442180ec8c6ec
Ojas Shirekar <ojas.shirekar@ojas.dev>
2025-01-23 15:02:38 +0100

    making metric executor

--- metrics/analyze.py
+++ metrics/analyze.py
@@ -1,5 +1,58 @@
-import tyro
-import rich
+from pathlib import Path
+from typing import List
+
+import einops
 import numpy as np
-import matplotlib.pyplot as plt
-from cmd_utils import ResultsTable
+import synchronization as sync
+import tyro
+import yaml
+from cmd_utils import Config, ResultsTable
+from numpy.typing import NDArray
+from wasabi import msg
+
+# fmt: off
+function_dict = {
+    name: getattr(sync, name) for name in dir(sync)
+    if callable(getattr(sync, name)) and not name.startswith("_")
+}
+# fmt: on
+
+
+def self_recurrence(
+    res_table: ResultsTable, metrics: List[str], data: NDArray, recurrence_radius: float, frames_first: bool = True
+) -> ResultsTable:
+    msg.info("Calculating Recurrence Matrix")
+    if frames_first:
+        data = einops.rearrange(data, "f d -> d f")
+    rec_matrix = sync.recurrence_matrix(data, radius=recurrence_radius)
+    for metric in metrics:
+        func = function_dict[metric]
+        if metric == "rqa_metrics":
+            rec, det, mean_length, max_length = func(rec_matrix)
+            res_table.add_result("Recurrence Rate", rec)
+            res_table.add_result("Determinism", det)
+            res_table.add_result("Mean Length", mean_length)
+            res_table.add_result("Max Length", max_length)
+            continue
+        res = func(rec_matrix)
+        res_table.add_result(metric, res)
+    return res_table
+
+
+def main(cfg_path: Path, npz_path: Path) -> int:
+    """
+    Analyze the synchronization metrics.
+    Args:
+        cfg_path: Path to the configuration file.
+    """
+    config = yaml.load(cfg_path.read_text(), Loader=yaml.FullLoader)
+    config = Config(**config)
+    data = np.load(npz_path.expanduser())
+    res_table = ResultsTable()
+    results = self_recurrence(res_table, config.metrics["recurrence_indiv"], data, config.recurrence_radius)
+    results.show()
+    return 0
+
+
+if __name__ == "__main__":
+    tyro.cli(main)
--- metrics/cmd_utils.py
+++ metrics/cmd_utils.py
@@ -1,9 +1,12 @@
 from rich.table import Table
-from typing import SupportsFloat
+from rich.console import Console
+from typing import List, SupportsFloat, Dict
+from dataclasses import dataclass
 
-class ResultsTable():
-    def __init__(self, results):
-        self.table = Table(title="Results")
+
+class ResultsTable:
+    def __init__(self, title="Results"):
+        self.table = Table(title=title)
 
     def add_columns(self):
         self.table.add_column("Metric", justify="left", style="cyan", no_wrap=True)
@@ -11,3 +14,13 @@ class ResultsTable():
 
     def add_result(self, metric: str, value: SupportsFloat):
         self.table.add_row(metric, str(value))
+
+    def show(self):
+        console = Console()
+        console.print(self.table)
+
+
+@dataclass
+class Config:
+    metrics: Dict[str, List[str]]
+    recurrence_radius: float
