65707ff1c538ee1d0d4c1a6d0944bd171a04df08
Ojas Shirekar <ojas.shirekar@pm.me>
2025-02-12 11:21:15 +0100

    updated deps and metrics

--- metrics/analyze.py
+++ metrics/analyze.py
@@ -1,11 +1,13 @@
+from copy import deepcopy
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 import einops
 import numpy as np
 import synchronization as sync
 import tyro
 import yaml
+from tqdm.rich import trange, tqdm
 from cmd_utils import Config, ResultsTable
 from numpy.typing import NDArray
 from wasabi import msg
@@ -18,25 +20,48 @@ function_dict = {
 # fmt: on
 
 
-def self_recurrence(
-    res_table: ResultsTable, metrics: List[str], data: NDArray, recurrence_radius: float, frames_first: bool = True
-) -> ResultsTable:
+class BasicRQA:
+    def __init__(self, recurrence_radius: float):
+        self.reccurence_radius = recurrence_radius
+
+    def calculate_rec_matrix(self, data):
+        return sync.recurrence_matrix(data, radius=self.recurrence_radius)
+
+    def calculate_rqa_metrics(self, rec_matrix):
+        return sync.rqa_metrics(rec_matrix)
+
+
+def joint_level_self_recurrence(
+    metrics: List[str],
+    joints: List[str],
+    data: Dict[str, NDArray],
+    recurrence_radius: float,
+    frames_first: bool = True,
+) -> List[ResultsTable]:
+    results = []
+    d = data
+    msg.divider("Joint Level Self Recurrence Analysis")
     msg.info("Calculating Recurrence Matrix")
-    if frames_first:
-        data = einops.rearrange(data, "f d -> d f")
-    rec_matrix = sync.recurrence_matrix(data, radius=recurrence_radius)
-    for metric in metrics:
-        func = function_dict[metric]
-        if metric == "rqa_metrics":
-            rec, det, mean_length, max_length = func(rec_matrix)
-            res_table.add_result("Recurrence Rate", rec)
-            res_table.add_result("Determinism", det)
-            res_table.add_result("Mean Length", mean_length)
-            res_table.add_result("Max Length", max_length)
-            continue
-        res = func(rec_matrix)
-        res_table.add_result(metric, res)
-    return res_table
+
+    for joint in (pbar := tqdm(joints)):
+        pbar.set_description(f"Processing joint: {joint}")
+        res_table = ResultsTable(title=joint)
+        if frames_first:
+            data = einops.rearrange(d[joint], "f d -> d f")
+        rec_matrix = sync.recurrence_matrix(data, radius=recurrence_radius)
+        for metric in metrics:
+            func = function_dict[metric]
+            if metric == "rqa_metrics":
+                rec, det, mean_length, max_length = func(rec_matrix)
+                res_table.add_result("Recurrence Rate", rec)
+                res_table.add_result("Determinism", det)
+                res_table.add_result("Mean Length", mean_length)
+                res_table.add_result("Max Length", max_length)
+                continue
+            res = func(rec_matrix)
+            res_table.add_result(metric, res)
+        results.append(res_table)
+    return results
 
 
 def main(cfg_path: Path, npz_path: Path) -> int:
@@ -48,9 +73,12 @@ def main(cfg_path: Path, npz_path: Path) -> int:
     config = yaml.load(cfg_path.read_text(), Loader=yaml.FullLoader)
     config = Config(**config)
     data = np.load(npz_path.expanduser())
-    res_table = ResultsTable()
-    results = self_recurrence(res_table, config.metrics["recurrence_indiv"], data, config.recurrence_radius)
-    results.show()
+
+    results = joint_level_self_recurrence(
+        config.metrics["recurrence_indiv"], config.joints, data, config.recurrence_radius
+    )
+    for res_table in results:
+        res_table.show()
     return 0
 
 
--- metrics/cmd_utils.py
+++ metrics/cmd_utils.py
@@ -2,14 +2,18 @@ from rich.table import Table
 from rich.console import Console
 from typing import List, SupportsFloat, Dict
 from dataclasses import dataclass
+from rich_tools import table_to_df
+from pathlib import Path
+from pandas import DataFrame
 
 
 class ResultsTable:
     def __init__(self, title="Results"):
         self.table = Table(title=title)
+        self.add_columns()
 
     def add_columns(self):
-        self.table.add_column("Metric", justify="left", style="cyan", no_wrap=True)
+        self.table.add_column("Metric", justify="left", style="cyan")
         self.table.add_column("Value", justify="right", style="magenta")
 
     def add_result(self, metric: str, value: SupportsFloat):
@@ -19,8 +23,17 @@ class ResultsTable:
         console = Console()
         console.print(self.table)
 
+    def to_df(self) -> DataFrame:
+        return table_to_df(self.table)
+
+    def save_csv(self, path: Path):
+        path = path.expanduser()
+        df = self.to_df()
+        df.to_csv(path)
+
 
 @dataclass
 class Config:
+    joints: List[str]
     metrics: Dict[str, List[str]]
     recurrence_radius: float
