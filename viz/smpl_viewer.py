b7812fa0f23923f945198ac7c53ffb09a62429b9
Ojas Shirekar <ojas.shirekar@pm.me>
2025-06-20 00:39:00 +0200

    wip: working smplx viewer

--- viz/smpl_viewer.py
+++ viz/smpl_viewer.py
@@ -12,13 +12,16 @@ from aitviewer.renderables.smpl import SMPLSequence
 from aitviewer.viewer import Viewer
 
 from kintree_constants import BODY_HAND_KINTREE, DND_BODY_HAND_KINTREE
+from pyprojroot import here
 
-C.smplx_models = "/Users/ojas/projects/smplx_models/"
+C.smplx_models = here() / "smplx"
 C.window_type = "pyglet"
 C.auto_set_floor = False
 
 
-def load_in_aitviewier(smpl_path: Path, kp_path: Path, csv_path: Path):  # pc_folder: Path, smpl_folder: Path):
+def load_in_aitviewier(
+    smpl_path: Path, kp_path: Path, csv_path: Path, frame_limit: int = 1000
+):  # pc_folder: Path, smpl_folder: Path):
     smplx_layer = SMPLLayer(model_type="smplx", gender="neutral", device=C.device)
 
     # To load a SMPL layer (without X), use this poses_body_end and change the model_type above to "smpl"
@@ -32,9 +35,7 @@ def load_in_aitviewier(smpl_path: Path, kp_path: Path, csv_path: Path):  # pc_fo
 
     smpl_seqs = []
 
-    for root_str, _, files in list(
-            os.walk(smpl_path)
-    ):
+    for root_str, _, files in list(os.walk(smpl_path)):
         root = Path(root_str)
         for filename in files:
             if filename.endswith(".npz"):
@@ -45,23 +46,19 @@ def load_in_aitviewier(smpl_path: Path, kp_path: Path, csv_path: Path):  # pc_fo
                 smpl_seqs.append(
                     SMPLSequence(
                         smpl_layer=smplx_layer,
-                        poses_body=data["poses"][:, 3:poses_body_end],
-                        poses_root=data["poses_root"][:, :3],
-                        betas=data["betas"],
-                        trans=data["trans"],
-                        poses_left_hand=data["poses"][:, poses_left_hand_start:poses_left_hand_end],
-                        poses_right_hand=data["poses"][:, poses_right_hand_start:],
+                        poses_body=data["poses"][:frame_limit, 3:poses_body_end],
+                        poses_root=data["poses_root"][:frame_limit, :3],
+                        betas=data["betas"][:frame_limit],
+                        trans=data["trans"][:frame_limit],
+                        poses_left_hand=data["poses"][:frame_limit, poses_left_hand_start:poses_left_hand_end],
+                        poses_right_hand=data["poses"][:frame_limit, poses_right_hand_start:],
                     )
                 )
 
     point_clouds = []
     points = []
 
-    for root_str, _, files in list(
-            os.walk(
-                kp_path
-            )
-    ):
+    for root_str, _, files in list(os.walk(kp_path)):
         root = Path(root_str)
         for filename in sorted(files):
             if filename.endswith(".json"):
@@ -110,7 +107,7 @@ def load_in_aitviewier(smpl_path: Path, kp_path: Path, csv_path: Path):  # pc_fo
 
 
 def add_body25_skeleton(
-        points: np.ndarray, icon="skeleton", kintree=BODY_HAND_KINTREE, color=(1.0, 0, 1 / 255, 1.0)
+    points: np.ndarray, icon="skeleton", kintree=BODY_HAND_KINTREE, color=(1.0, 0, 1 / 255, 1.0)
 ) -> Skeletons:
     skeleton = Skeletons(
         joint_positions=points[:, :, :3],
